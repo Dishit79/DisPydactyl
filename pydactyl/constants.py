6f61bcd625714be46ded8411f392b2e84a4a3861
Dishit79 <56460773+Dishit79@users.noreply.github.com>
2021-01-03 22:14:30 -0500

    Update constants.py

--- pydactyl/constants.py
+++ pydactyl/constants.py
@@ -2,7 +2,7 @@
 
 __version__ = '0.1.13'
 
-USER_AGENT = 'Pydactyl/' + __version__
+USER_AGENT = 'Dispydactyl/' + __version__
 POWER_SIGNALS = ('start', 'stop', 'restart', 'kill')
 USE_SSL = {True: 'https', False: 'http'}
 REQUEST_TYPES = ('GET', 'POST', 'PATCH', 'DELETE')
